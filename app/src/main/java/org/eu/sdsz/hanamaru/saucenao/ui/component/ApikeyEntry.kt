4173104849d848513ae3239c33f5a9aed5f5605d
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-05-15 20:56:26 +0800

    Add PasswordField component

---
+++ app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ApikeyEntry.kt
@@ -0,0 +1,41 @@
+package org.eu.sdsz.hanamaru.saucenao.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PasswordField(value: String, onValueChange: (String)->Unit) {
+    var visible by rememberSaveable {
+        mutableStateOf(false)
+    }
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        visualTransformation = if (visible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            IconButton(onClick = { visible = !visible }) {
+                Icon(
+                    imageVector = if (visible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                    contentDescription = "Change Visibility"
+                )
+            }
+        }
+    )
+}
