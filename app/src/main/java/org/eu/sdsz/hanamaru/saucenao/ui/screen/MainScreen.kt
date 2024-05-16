652fd6f0579b49c0ed8bbbb9c1afdcf7eb7564cd
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-05-16 16:50:30 +0800

    Add MainScreen

---
+++ app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/screen/MainScreen.kt
@@ -0,0 +1,27 @@
+package org.eu.sdsz.hanamaru.saucenao.ui.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.eu.sdsz.hanamaru.saucenao.ui.component.ImagePicker
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            ImagePicker()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    MainScreen()
+}
