774d02b9f0165f5a946f8bcb3c7c16efdab3afdf
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-05-16 17:46:29 +0800

    Add ResultScreen

---
+++ app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/screen/ResultScreen.kt
@@ -0,0 +1,28 @@
+package org.eu.sdsz.hanamaru.saucenao.ui.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.eu.sdsz.hanamaru.saucenao.ui.component.ResultList
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun ResultScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopCenter) {
+            ResultList()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewResultScreen() {
+    ResultScreen()
+}
