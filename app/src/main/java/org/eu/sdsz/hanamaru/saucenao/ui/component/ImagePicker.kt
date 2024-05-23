e485b23ee852f985fcff0d021c6b5f959b0be0fd
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-05-23 17:32:57 +0800

    Improve ui of main screen

--- app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ImagePicker.kt
+++ app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ImagePicker.kt
@@ -3,8 +3,9 @@ package org.eu.sdsz.hanamaru.saucenao.ui.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
@@ -13,25 +14,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ImagePicker(method: Boolean, onMethodChange: (Boolean)->Unit, onSelectImage: ()->Unit, imageUrl: String, onUrlChange: (String)->Unit, onSearch: ()->Unit) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Column (
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Text(text = "Select search method")
+            Text(text = "Select search method", fontWeight = FontWeight.Bold, fontSize = 20.sp)
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.widthIn(64.dp,256.dp)
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = "Local image"
-                ,modifier = Modifier.weight(1f))
+                Text(
+                    text = "Local",
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.Right
+                )
+                Spacer(modifier = Modifier.width(6.dp))
                 Switch(
                     checked = method,
                     onCheckedChange = onMethodChange,
@@ -43,26 +52,23 @@ fun ImagePicker(method: Boolean, onMethodChange: (Boolean)->Unit, onSelectImage:
                     ),
                     modifier = Modifier.align(Alignment.CenterVertically)
                 )
-                Text("URL",
+                Spacer(modifier = Modifier.width(6.dp))
+                Text(
+                    text = "URL",
                     modifier = Modifier.weight(1f),
-                    textAlign = TextAlign.Right)
+                    textAlign = TextAlign.Left
+                )
             }
-        }
-
-
-        Column (
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
             if (method) {
                 ImageUrlEntry(url = imageUrl, onValueChange = onUrlChange)
             } else {
                 ImageFileSelector(onClick = onSelectImage)
             }
 
-            Button(onClick = onSearch) {
-                Text(text = "Search")
-            }
+        }
+
+        Button(onClick = onSearch) {
+            Text(text = "Search")
         }
     }
 }
