7ea7aa8173ed3124599ebc92c788fe61d38a9268
dedfaf <118916428+dedfaf@users.noreply.github.com>
2024-05-23 15:51:24 +0800

    Centered ImagePicker UI, Add Alignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.moshi.kotlin)
     implementation(libs.moshi)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.ui.test.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ImagePicker.kt
+++ app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ImagePicker.kt
@@ -1,41 +1,68 @@
 package org.eu.sdsz.hanamaru.saucenao.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ImagePicker(method: Boolean, onMethodChange: (Boolean)->Unit, onSelectImage: ()->Unit, imageUrl: String, onUrlChange: (String)->Unit, onSearch: ()->Unit) {
     Column {
-        Row {
-            Text(text = "Select a picture or input URL")
-            Switch(
-                checked = method,
-                onCheckedChange = onMethodChange,
-                colors = SwitchDefaults.colors(
-                    checkedThumbColor = MaterialTheme.colorScheme.secondary,
-                    checkedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
-                    uncheckedThumbColor = MaterialTheme.colorScheme.secondary,
-                    uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer
+        Column (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Select search method")
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.widthIn(64.dp,256.dp)
+            ) {
+                Text(text = "Local image"
+                ,modifier = Modifier.weight(1f))
+                Switch(
+                    checked = method,
+                    onCheckedChange = onMethodChange,
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = MaterialTheme.colorScheme.secondary,
+                        checkedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                        uncheckedThumbColor = MaterialTheme.colorScheme.secondary,
+                        uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer
+                    ),
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
-            )
+                Text("URL",
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.Right)
+            }
         }
 
 
-        if (method) {
-            ImageUrlEntry(url = imageUrl, onValueChange = onUrlChange)
-        } else {
-            ImageFileSelector(onClick = onSelectImage)
-        }
+        Column (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            if (method) {
+                ImageUrlEntry(url = imageUrl, onValueChange = onUrlChange)
+            } else {
+                ImageFileSelector(onClick = onSelectImage)
+            }
 
-        Button(onClick = onSearch) {
-            Text(text = "Search")
+            Button(onClick = onSearch) {
+                Text(text = "Search")
+            }
         }
     }
 }
