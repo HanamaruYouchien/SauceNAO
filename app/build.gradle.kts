70bbc2fc758b15fce860f9b0e8484fd11d2c05df
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-05-26 01:01:03 +0800

    Change version to 0.1.1-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.eu.sdsz.hanamaru.saucenao"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
