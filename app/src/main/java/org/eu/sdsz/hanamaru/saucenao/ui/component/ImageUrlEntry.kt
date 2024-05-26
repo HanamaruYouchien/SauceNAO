72dc50d8dbdc064d4c694a4c44c4f831f58ede4b
dedfaf <118916428+dedfaf@users.noreply.github.com>
2024-05-26 15:23:07 +0800

    Add clear iconbutton in ImageUrlEntry

--- app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ImageUrlEntry.kt
+++ app/src/main/java/org/eu/sdsz/hanamaru/saucenao/ui/component/ImageUrlEntry.kt
@@ -1,16 +1,30 @@
 package org.eu.sdsz.hanamaru.saucenao.ui.component
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ImageUrlEntry(url: String, onValueChange: (String)->Unit) {
-    TextField(value = url, onValueChange = onValueChange)
+    TextField(
+        value = url,
+        onValueChange = onValueChange,
+        trailingIcon = {
+            IconButton(onClick = { onValueChange("") } )  {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Clear input URL"
+                )
+            }
+        })
 }
 
 @Preview
 @Composable
 fun PreviewImageUrlEntry() {
-    ImageUrlEntry("") {}
+    ImageUrlEntry("https://news.cnr.cn/mryxh/images/banner.jpg") {}
 }
